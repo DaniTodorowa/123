b69a3fa11a98f94b48493741f0f41176b2332fb0
DaniTodorowa <danitodorova2106@gmail.com>
2021-01-07 22:16:37 +0200

    new

--- if-else-lab/greaterNumber.js
+++ if-else-lab/greaterNumber.js
@@ -1,11 +1,19 @@
+// function greaterNumber(num1, num2){
+//     num1 = Number(num1);
+//     num2 = Number(num2);
+//     if (num1 > num2) {
+//         console.log(num1);
+//
+//     }else {
+//         console.log(num2)
+//     }
+// }
+// greaterNumber(8, 3)
+
+
 function greaterNumber(num1, num2){
     num1 = Number(num1);
     num2 = Number(num2);
-    if (num1 > num2) {
-        console.log(num1);
-
-    }else {
-        console.log(num2)
-    }
+    console.log(Math.max(num1, num2))
 }
 greaterNumber(8, 3)
