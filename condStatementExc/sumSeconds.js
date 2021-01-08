eab71b60d0f8547cb98ebfcb4b17f55710f8a6e3
DaniTodorowa <danitodorova2106@gmail.com>
2021-01-09 00:17:30 +0200

    commit

--- condStatementExc/sumSeconds.js
+++ condStatementExc/sumSeconds.js
@@ -4,7 +4,7 @@ function sumSeconds(arg1, arg2, arg3) {
     let c = Number(arg3);
 
     let sumSeconds = (a + b + c);
-    let min = parseInt(sumSeconds / 60);
+    let min = Math.floor(sumSeconds / 60);
     let sec = sumSeconds % 60;
     if (sec < 10) {
         console.log(`${min}:0${sec}`);
