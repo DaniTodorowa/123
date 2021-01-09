9ae9fc47b2e3d7ff30949e1349a8bba41af147b9
DaniTodorowa <danitodorova2106@gmail.com>
2021-01-09 19:09:58 +0200

    input changed

--- firstStepsInCodding/fruitMarket.js
+++ firstStepsInCodding/fruitMarket.js
@@ -1,9 +1,15 @@
-function moneyNeeded(strawberryPrice, bananaKg, orangeKg, raspberryKg, strawberryKg){
-    const priceRaspberry = strawberryPrice/2
+function moneyNeeded(input){
+    let strawberryPrice = Number(input[0]);
+    let bananaKg = Number(input[1]);
+    let orangeKg = Number(input[2]);
+    let raspberryKg = Number(input[3]);
+    let strawberryKg = Number(input[4]);
+
+    const priceRaspberry = Number(strawberryPrice/2);
     const priceOrange = priceRaspberry - priceRaspberry*0.4
     const  priceBanana = priceRaspberry - priceRaspberry * 0.8
     const res = (strawberryPrice * strawberryKg + bananaKg * priceBanana + priceRaspberry * raspberryKg + priceOrange * orangeKg)
-    console.log(res)
+    console.log(res.toFixed(2))
 }
 
-moneyNeeded("63.5", "3.57", "6.35", "8.15", "2.5")
+moneyNeeded(["48", "10", "3.3", "6.5","1.7"])
