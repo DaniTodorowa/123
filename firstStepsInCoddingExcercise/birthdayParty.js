5bdd001226d63076426bec7267d0a0950704ff68
DaniTodorowa <danitodorova2106@gmail.com>
2021-01-09 21:15:28 +0200

    commit

--- firstStepsInCoddingExcercise/birthdayParty.js
+++ firstStepsInCoddingExcercise/birthdayParty.js
@@ -1,9 +1,9 @@
 function budget(rentInput){
     const rentHall = parseInt(rentInput)
     const cake = rentHall*20/100
-    const bevarages = cake - cake*0.45
+    const beverages = cake - cake*0.45
     const animator = rentHall/3
-    const res = ((rentHall) + (cake) + (animator)+ (bevarages))
+    const res = ((rentHall) + (cake) + (animator)+ (beverages))
     console.log(res)
 }
  budget("3720")
