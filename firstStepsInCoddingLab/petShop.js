7af1852f6b9ecfa1a2c8d723dc348e1857c61aec
DaniTodorowa <danitodorova2106@gmail.com>
2021-01-09 18:49:31 +0200

    commit

---
+++ firstStepsInCoddingLab/petShop.js
@@ -0,0 +1,7 @@
+function petShop(input){
+    let dogsCount = Number(input[0]);
+    let othersAnimals = Number(input[1]);
+    console.log(`${dogsCount*2.5 + othersAnimals*4} lv.`)
+}
+
+petShop(["5","4"])
