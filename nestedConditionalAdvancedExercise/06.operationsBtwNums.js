2d52d362efdeb3adde0cc3b03e7f80081b431107
DaniTodorowa <danitodorova2106@gmail.com>
2021-01-10 23:53:23 +0200

    commit

---
+++ nestedConditionalAdvancedExercise/06.operationsBtwNums.js
@@ -0,0 +1,43 @@
+function solve(input){
+    let N1 = Number(input[0]);
+    let N2 = Number(input[1]);
+    let operator = input[2];
+    let result = 0;
+    if (N2 === 0){
+        console.log(`Cannot divide ${N1} by zero`)
+    }else {
+        if (operator === "%"){
+            result = N1 % N2
+            console.log(`${N1} % ${N2} = ${result}`);
+        }else if (operator === "/"){
+            result = N1 / N2
+            console.log(`${N1} / ${N2} = ${result.toFixed(2)}`);
+        }else if (operator === "+" ){
+            result = N1 + N2
+            if (result % 2 ===0){
+                console.log(`${N1} + ${N2} = ${result} - even`);
+            }else {
+                console.log(`${N1} + ${N2} = ${result} - odd`);
+            }
+
+        }else if (operator === "-" ){
+            result = N1 - N2
+            if (result % 2 ===0){
+                console.log(`${N1} - ${N2} = ${result} - even`);
+            }else {
+                console.log(`${N1} - ${N2} = ${result} - odd`);
+            }
+
+        }else if (operator === "*" ){
+            result = N1 * N2
+            if (result % 2 ===0){
+                console.log(`${N1} * ${N2} = ${result} - even`);
+            }else {
+                console.log(`${N1} * ${N2} = ${result} - odd`);
+            }
+
+        }
+    }
+}
+
+solve(['10', '0', '%'])
