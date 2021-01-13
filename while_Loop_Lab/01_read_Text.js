d184fd50fc2c69df3b3041bab0356b817ccace12
DaniTodorowa <danitodorova2106@gmail.com>
2021-01-13 17:13:31 +0200

    commit

---
+++ while_Loop_Lab/01_read_Text.js
@@ -0,0 +1,17 @@
+function solve(input){
+    let elements = input;
+    while (true){
+        for (i =0; i<elements.length;i++){
+            if (elements[i] === 'Stop') {
+                {
+                    break;
+                }
+            }else {
+                console.log(elements[i]);
+
+            }
+        }{break;}
+    }
+}
+
+solve(['SDD', "PPP", "Stop", 'DDFD'])
