4763213766133dacbfe5fae4bcdbb8f949fc86df
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-05 22:35:41 +0200

    commit

---
+++ FUNDAMENTALS/basicSyntaxConditionsLoops/11_sum_of_odd_nums.js
@@ -0,0 +1,14 @@
+function solve(n) {
+    let sum = 0;
+    let num = 1
+    for (let i = 1; i<=n; i++){
+        console.log(num);
+        sum+= num
+        num +=2;
+
+
+    }
+    console.log(`Sum: ${sum}`);
+}
+
+solve(3)
