97c0a05b29f4151d809dc289ed404cd37a493304
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-06 21:52:05 +0200

    commit

--- FUNDAMENTALS/dataTypesAndVariables_lab/10_triples_of_latin_letters.js
+++ FUNDAMENTALS/dataTypesAndVariables_lab/10_triples_of_latin_letters.js
@@ -1,13 +1,13 @@
 function solve(num) {
 
-    for (let first = 0;first<3; first++){
+    for (let first = 0;first<num; first++){
 
         let letter1 = String.fromCharCode(97 + first);
 
-        for (let sec = 0;sec<3; sec++){
+        for (let sec = 0;sec<num; sec++){
             let letter2 = String.fromCharCode(97 + sec);
 
-            for (let third = 0;third<3; third++){
+            for (let third = 0;third<num; third++){
 
                 let letter3 = String.fromCharCode(97 + third);
 
