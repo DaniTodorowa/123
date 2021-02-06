08b9a995e0882515416dfd178a54df3fe418ace8
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-06 20:57:25 +0200

    commit

--- FUNDAMENTALS/dataTypesAndVariables_lab/06_fuel_money.js
+++ FUNDAMENTALS/dataTypesAndVariables_lab/06_fuel_money.js
@@ -1,10 +1,25 @@
-function solve(distance, passengers, price) {
-    let neededFuel = (distance/100*7);
-    neededFuel+= passengers*.1;
-    let money = neededFuel*price;
-    //  let fuel = (distance*(7/100 )) + (passengers*100/1000);
-    //  let res = price*fuel;
-    console.log(`Needed money for that trip is ${money}lv.`);
-}
+// function solve(distance, passengers, price) {
+//      let neededFuel = (distance/100*7);
+//     neededFuel+= passengers*.1;
+//      let money = neededFuel*price;
+//      let fuel = (distance*(7/100 )) + (passengers*100/1000);
+//      let res = price*fuel;
+//     console.log(`Needed money for that trip is ${money}lv.`);
+//
+// }
+//
+// solve(260,
+// 9,
+// 2.49)
 
-solve(90, 14, 2.88)
+function solve(a, b, c) {
+    let distance = Number(a);
+    let passengers = Number(b);
+    let price = Number(c);
+    let fuel = distance*7/100 + (passengers*100/1000);
+    let res = price*fuel;
+    console.log(`Needed money for that trip is ${res}lv.`);
+}
+solve(260,
+    9,
+    2.49)
