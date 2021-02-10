5497c3f5c33c2408c7f2e7b5075538b4d1904718
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-10 16:14:41 +0200

    commit

---
+++ FUNDAMENTALS/objects_and_classes/01_peson_info.js
@@ -0,0 +1,14 @@
+function solve(firstName, lastName, age) {
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = Number(age);
+    for (let key in person){
+        console.log(`${key}: ${person[key]}`);
+    }
+}
+
+solve("Peter",
+    "Pan",
+    "20"
+)
