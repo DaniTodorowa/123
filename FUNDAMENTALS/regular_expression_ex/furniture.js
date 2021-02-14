0474cdfd89e0a7ad29355a1363247fd97a0beb07
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-14 23:34:19 +0200

    commit

---
+++ FUNDAMENTALS/regular_expression_ex/furniture.js
@@ -0,0 +1,24 @@
+function solve(input) {
+    let boughtFurniture = [];
+    let totalMoneySpend = 0;
+    for (let data of input){
+        let match = />>(?<name>[A-Za-z]+)<<(?<price>[\d]+\.?[\d]*)!(?<quantity>[\d]+)/g.exec(data);
+        //console.log(match);
+        if(match){
+            boughtFurniture.push(match.groups.name);
+            totalMoneySpend+= Number(match.groups.price)* Number(match.groups.quantity);
+
+        }
+    }
+    console.log('Bought furniture:');
+    if (boughtFurniture.length>0) {
+        console.log(boughtFurniture.join('\n'));
+        console.log(`Total money spend: ${totalMoneySpend.toFixed(2)}`);
+    }
+
+}
+
+solve(['>>Sofa<<312.23!3',
+'>>TV<<300!5',
+'Purchase']
+)
