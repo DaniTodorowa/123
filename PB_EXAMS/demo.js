813f04667e25a329c041851c55dad701bab0aaef
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-18 21:37:54 +0200

    commit

--- PB_EXAMS/demo.js
+++ PB_EXAMS/demo.js
@@ -1,3 +1,49 @@
+function solve(input){
+    let quantity = Number(input.shift());
+    let command = input.shift();
+    let sold = 0;
+    let isSold = false;
+    while (true){
+
+        if (command === 'Close'){
+            break;
+
+        }else {
+            let currentQuantity = Number(input.shift());
+            if (command ==="Buy"){
+                if (currentQuantity>quantity){
+                    console.log("Not enough eggs in store!");
+                    console.log(`You can buy only ${quantity}.`);
+                    isSold = true;
+                    break;
+                }else {
+                    quantity-=currentQuantity;
+                    sold+=currentQuantity;
+                }
+            }else if (command === 'Fill'){
+                quantity+=currentQuantity;
+
+            }
+        }
+        command = input.shift();
+    }
+    if (!isSold){
+        console.log('Store is closed!');
+        console.log(`${sold} eggs sold.`);
+    }
+
+}
+
+solve([
+
+        '20', 'Fill',
+        '30', 'Buy',
+        '15', 'Buy',
+        '20', 'Close'
+    ]
+)
+
+//second solving
 function solve(input) {
     let eggs = Number(input.shift());
     let data = input.shift();
@@ -26,12 +72,3 @@ function solve(input) {
         console.log(`You can buy only ${eggs}.`);
     }
 }
-
-solve([
-
-        '20', 'Fill',
-        '30', 'Buy',
-        '15', 'Buy',
-        '20', 'Close'
-    ]
-)
