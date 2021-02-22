94ae4811488137cb6dfec83a0fa958c1bd9a7763
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-22 18:57:18 +0200

    commit

--- FUNDAMENTALS/dataTypesAndVariables_lab/07_centuries_to_minutes.js
+++ FUNDAMENTALS/dataTypesAndVariables_lab/07_centuries_to_minutes.js
@@ -1,8 +1,8 @@
 function solve(num) {
     let years = num*100;
-    let days = Math.round(years*365.2422);
-    let hours = Math.round(days*24);
-    let min = Math.round(hours*60);
+    let days = Math.trunc(years*365.2422);
+    let hours = days*24;
+    let min = hours*60;
     console.log(`${num} centuries = ${years} years = ${days} days = ${hours} hours = ${min} minutes`);
 }
 
