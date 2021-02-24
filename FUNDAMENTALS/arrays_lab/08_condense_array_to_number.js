43a2d4ab4e91965f92f8098b6ab832fb1169e626
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-24 21:50:16 +0200

    commit

--- FUNDAMENTALS/arrays_lab/08_condense_array_to_number.js
+++ FUNDAMENTALS/arrays_lab/08_condense_array_to_number.js
@@ -1,8 +1,14 @@
 function solve(input) {
-    let res = [];
-    while (input.length >1){
-
+    let numbers = input;
+    let condensed = [];
+    while (numbers.length > 1) {
+        for (let i = 0; i < numbers.length - 1; i++) {
+            condensed.push(numbers[i] + numbers[i + 1]);
+        }
+        numbers = condensed;
+        condensed = [];
     }
+    console.log(numbers[0]);
 }
 
-solve([2,10,3])
+solve([2, 10, 3])
