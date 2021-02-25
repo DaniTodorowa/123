e2ea12123e28840bacb87d2a17adcc5c8760ca23
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-25 18:01:35 +0200

    commit

---
+++ FUNDAMENTALS/arrays_exc/02_common_elements_2.js
@@ -0,0 +1,10 @@
+function solve(arrayOne =[], arrayTwo =[]) {
+    arrayOne.map((element)=> {
+        if (arrayTwo.includes(element)){
+            console.log(element);
+        }
+    });
+}
+
+solve(['Hey', 'hello', 2, 4, 'Peter', 'e'],
+    ['Petar', 10, 'hey', 4, 'hello', '2'])
