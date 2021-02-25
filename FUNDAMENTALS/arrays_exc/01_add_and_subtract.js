848e75b655f2c853902faaa454528af737eca251
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-25 17:15:38 +0200

    commit

---
+++ FUNDAMENTALS/arrays_exc/01_add_and_subtract.js
@@ -0,0 +1,14 @@
+function solve(data = []) {
+    let resultArray = [];
+    let originalSum = 0;
+    let resultSum = 0;
+
+    data.map((number, index) => {
+        number % 2 === 0 ? number += index : number -= index;
+
+        resultArray.push(number);
+    });
+    console.log(resultArray);
+    console.log(originalSum = data.reduce((a, b) => a + b, 0));
+    console.log(resultSum = resultArray.reduce((a, b) => a + b, 0));
+}
