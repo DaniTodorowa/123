2c0ef3185449b07732656582180d7d8fff99ef67
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-28 18:34:29 +0200

    commit

---
+++ FUNDAMENTALS/funcions_exc/07_NxN_matrix.js
@@ -0,0 +1,11 @@
+function solve(num) {
+  for (let n =0;n<num;n++){
+      let res = [];
+      for (let j = 0;j<num;j++){
+          res.push(num);
+      }
+      console.log(res.join(" "));
+  }
+}
+
+solve(2)
