2cc120b4ee8cc15bc2d848b2ccbb7e940746f48c
DaniTodorowa <danitodorova2106@gmail.com>
2021-02-28 20:58:11 +0200

    commit

---
+++ FUNDAMENTALS/funcions_exc/10_factorial_division.js
@@ -0,0 +1,13 @@
+function solve(firs,second) {
+    let firstRes =1;
+    let secRes = 1;
+    for (let i =firs;i>0;i--){
+        firstRes*= i;
+    }
+    for (let j =second;j>0;j--){
+        secRes *= j;
+    }
+    console.log((firstRes/secRes).toFixed(2));
+}
+
+solve(6,2)
