2efeaf97883bc8c73014443f52077906432ffc9c
DaniTodorowa <danitodorova2106@gmail.com>
2021-03-20 15:15:22 +0200

    commit

--- FUNDAMENTALS/arrays_lab/06_even_and_odd_subtaction.js
+++ FUNDAMENTALS/arrays_lab/06_even_and_odd_subtaction.js
@@ -15,3 +15,30 @@ function solve(input) {
 }
 
 solve([1,2,3,4,5,6])
+
+// function solve(input) {
+//     function evenSum(arr1){
+//         let evenSum = 0;
+//         arr1.forEach(x=>{
+//             (x%2===0 ? evenSum+= x : undefined)
+//         });
+//         return evenSum;
+//     }
+//
+//     function oddSum(arr1){
+//         let oddSum = 0;
+//         arr1.forEach(x=>{
+//             (x%2!==0 ? oddSum+= x : undefined)
+//         });
+//         return oddSum;
+//     }
+//
+//     function calcDiff(a,b){
+//         return a - b;
+//     }
+//     // let evenSumRes = evenSum(input);
+//     // let oddSumRes = oddSum(input);
+//     return calcDiff(evenSum(input), oddSum(input))
+// }
+//
+// console.log(solve([1,2,3,4,5,6]));
