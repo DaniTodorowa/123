2adda231f4db99e23e994c5d59b131d517b6f855
DaniTodorowa <danitodorova2106@gmail.com>
2021-03-24 21:06:27 +0200

    75/100 in Judge

--- FUNDAMENTALS/object_and_classes_exercise/08_class_storage_mine.js
+++ FUNDAMENTALS/object_and_classes_exercise/08_class_storage_mine.js
@@ -2,29 +2,32 @@ class Storage {
     constructor(capacity) {
         this.capacity = capacity;
         this.storage = [];
+        this.totalCost =0;
 
     }
 
-    addProduct(objProduct) {
-        this.storage.push(objProduct);
-        this.capacity -= objProduct['quantity']//Object.values(objProduct)[2]
+    addProduct(product) {
+        this.storage.push(product);
+        this.capacity -= product.quantity;//Object.values(objProduct)[2]
+        this.totalCost += product.price * product.quantity;
+
     }
 
     getProducts(product) {
         this.storage.forEach(p => {
-            console.log( (JSON.stringify(p)));
+            return JSON.stringify(p);
         });
     }
 
-    get totalCost() {
-        let totalCost = 0;
-        this.storage.forEach(obj => {
-            let valuesArr = Object.values(obj);
-            totalCost += valuesArr[1] * valuesArr[2];
-            //console.log(sum);
-        });
-        return totalCost;
-    }
+    // get totalCost() {
+    //     let totalCost = 0;
+    //     this.storage.forEach(obj => {
+    //         let valuesArr = Object.values(obj);
+    //         totalCost += valuesArr[1] * valuesArr[2];
+    //         //console.log(sum);
+    //     });
+    //     return totalCost;
+    // }
 }
 
 let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
