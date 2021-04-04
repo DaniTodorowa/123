2a0130364c96da5cc9697917efc16a98dba3e7ac
DaniTodorowa <danitodorova2106@gmail.com>
2021-04-04 13:13:32 +0300

    commit

--- JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/07_validity_checke_mine.js
+++ JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/07_validity_checke_mine.js
@@ -6,16 +6,19 @@ function solve(arr =[]) {
     let distanceTwoPoints = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     let distancePointCenterX1Y1 = Math.sqrt(Math.pow((0 - x1), 2) + Math.pow((0 - y1), 2));
     let distancePointCenterX2Y2 = Math.sqrt(Math.pow((0 - x2), 2) + Math.pow((0 - y2), 2));
+
     if (Number.isInteger(distancePointCenterX1Y1)) {
         console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
     } else {
         console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
     }
+
     if (Number.isInteger(distancePointCenterX2Y2)) {
         console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
     } else {
         console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
     }
+
     if (Number.isInteger(distanceTwoPoints)) {
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
     } else {
