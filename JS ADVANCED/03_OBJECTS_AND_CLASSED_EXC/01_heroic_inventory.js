54a27af96372ccbc49e50aaef4019d64b3ffff31
DaniTodorowa <danitodorova2106@gmail.com>
2021-04-06 11:54:55 +0300

    80/100 in judge

---
+++ JS ADVANCED/03_OBJECTS_AND_CLASSED_EXC/01_heroic_inventory.js
@@ -0,0 +1,14 @@
+function solve(input) {
+    let heroes = [];
+    input.forEach(line => {
+        let [name, level, items] = line.split(" / ");
+        level = Number(level)
+        items = items.split(", ");
+        heroes.push({name, level, items})
+
+
+    });
+    console.log(JSON.stringify(heroes));
+}
+
+solve(['Jake / 1000 / Gauss, HolidayGrenade'])
