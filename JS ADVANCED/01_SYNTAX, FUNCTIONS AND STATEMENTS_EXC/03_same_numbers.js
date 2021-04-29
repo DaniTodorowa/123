1c404ba54c251878ebe758312e84432773c00c86
DaniTodorowa <danitodorova2106@gmail.com>
2021-04-29 18:15:07 +0300

    commit

--- JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/03_same_numbers.js
+++ JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/03_same_numbers.js
@@ -13,3 +13,18 @@ function solve(num) {
 }
 
 console.log(solve(1234));
+
+// function solve(input) {
+//     let inputStrArr = input.toString().split("");
+//     //console.log(inputStrArr);
+//     let sumDigits = inputStrArr.map(Number).reduce((a,c)=>a+c,0);
+//
+//     if(sumDigits/inputStrArr.length === Number(inputStrArr[0])){
+//         console.log("true");
+//     }else {
+//         console.log("false");
+//     }
+//     console.log(sumDigits)
+// }
+//
+// solve(1234)
