602bc2625c9f28efdb4249d8e48e5abddc2fb5bf
DaniTodorowa <danitodorova2106@gmail.com>
2021-04-29 21:25:23 +0300

    88/100 in judge

--- JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/05_road_radar.js
+++ JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/05_road_radar.js
@@ -1,45 +1,73 @@
-function solve(input) {
-    let typeRoad = input[1];
-    let speed = input[0];
+function solve(a,b) {
+    let typeRoad = b;
+    let speed = a;
     let diff = 0;
+    let speedLimit = 0;
+    let status = '';
     if (typeRoad === 'city') {
+
         diff = speed - 50;
+        speedLimit = 50
         if (diff > 0 && diff <= 20) {
-            return `speeding `;
+            status = 'speeding'
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 20 && diff < 40) {
-            return "excessive speeding";
+            status = "excessive speeding"
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 40) {
-            return "reckless driving ";
+            status = "reckless driving ";
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+        }
+        else{
+            console.log( `Driving ${speed} km/h in a ${speedLimit} zone`);
         }
 
     } else if (typeRoad === 'residential') {
+        speedLimit = 20;
         diff = speed - 20;
         if (diff > 0 && diff <= 20) {
-            return `speeding `;
+            status = `speeding`
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 20 && diff < 40) {
-            return "excessive speeding";
+            status = "excessive speeding";
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 40) {
-            return "reckless driving ";
+            status = "reckless driving ";
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+        }else {
+            console.log( `Driving ${speed} km/h in a ${speedLimit} zone`);
         }
     } else if (typeRoad === 'interstate') {
+        speedLimit = 90;
         diff = speed - 90;
         if (diff > 0 && diff <= 20) {
-            return `speeding `;
+            status = `speeding`;
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 20 && diff < 40) {
-            return "excessive speeding";
+            status = "excessive speeding";
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 40) {
-            return "reckless driving ";
+            status ="reckless driving ";
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+        }else {
+            console.log( `Driving ${speed} km/h in a ${speedLimit} zone`);
         }
     } else if (typeRoad === 'motorway') {
+        speedLimit = 130;
         diff = speed - 130;
+        status = `speeding`
         if (diff > 0 && diff <= 20) {
-            return `speeding `;
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 20 && diff < 40) {
-            return "excessive speeding";
+            status = "excessive speeding";
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
         } else if (diff > 40) {
-            return "reckless driving ";
+            status = "reckless driving"
+            console.log( `The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+        }else{
+            console.log( `Driving ${speed} km/h in a ${speedLimit} zone`);
         }
     }
 }
 
-console.log(solve([200, 'motorway']));
+(solve(40, 'city'));
