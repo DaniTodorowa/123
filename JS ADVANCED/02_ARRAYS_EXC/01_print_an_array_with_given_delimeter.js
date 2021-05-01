6c4c93ecb0464b858ae1ee03eb7df36ad52a7684
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-01 21:13:36 +0300

    commit

--- JS ADVANCED/02_ARRAYS_EXC/01_print_an_array_with_given_delimeter.js
+++ JS ADVANCED/02_ARRAYS_EXC/01_print_an_array_with_given_delimeter.js
@@ -1,16 +1,11 @@
-function solve(input) {
-
-    let delimiter = input[input.length-1];
-    let arr = input.slice(0, input.length-1)
-    return arr.join(`${delimiter}`)
+function solve(arr=[],delimiter) {
+    console.log(arr.join(`${delimiter}`));
 }
 
-console.log(solve(['How about no?',
-    'I',
-    'will',
-    'not',
-    'do',
-    'it!',
-    '_'
-
-]));
+solve(['One',
+        'Two',
+        'Three',
+        'Four',
+        'Five'],
+    '-'
+)
