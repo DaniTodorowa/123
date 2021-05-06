7057c2ecea6d27a383785da909ab6162fae167b2
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-06 18:45:05 +0300

    commit

--- JS ADVANCED/03_OBJECTS_AND_COMPOSITION_EXC/04_store_catalogue.js
+++ JS ADVANCED/03_OBJECTS_AND_COMPOSITION_EXC/04_store_catalogue.js
@@ -1,19 +1,22 @@
-function solve(input) {
-    let obj ={};
-    input.forEach(line=>{
-        let [item, price] = line.split(" : ");
-        let firstLetter = item[0];
-        !firstLetter.hasOwnProperty(obj) ? obj[firstLetter] = {[item]: price}: 'pass';
-    });
-    console.log(obj);
-}
-solve(['Appricot : 20.4',
-    'Fridge : 1500',
-    'TV : 1499',
-    'Deodorant : 10',
-    'Boiler : 300',
-    'Apple : 1.25',
-    'Anti-Bug Spray : 15',
-    'T-Shirt : 10']
+function storeCatalogue(input) {
+    // Mask - https://git.io/JfreS
+    let catalogue = {};
+    input.map(data => { catalogue[data.split(' : ')[0]] = Number(data.split(' : ')[1]); });
 
-)
+    let letter = '' // Here We will keep the current letter to Print...
+
+    // The sort method doesn't require a second argument for sort by value BUT it it's for testing :).
+    // First, we will create Array from the KEYS of the Catalogue.
+    Array.from(Object.keys(catalogue))
+        // Then we will sort the ARRAY First Alphabetically and then if the names are the same: by value
+        .sort((a, b) => { return a.localeCompare(b) || catalogue[a] - catalogue[b] })
+        // And the we iterate the SORTED ARRAY element by element to print it.
+        .map(product => {
+            // But first we will check if the letter need's to be printed:
+            if (product[0] !== letter) {
+                letter = product[0];
+                console.log(product[0]);
+            }
+            console.log(`  ${product}: ${catalogue[product]}`);  // And Finally we will print the output line.
+        });
+}
