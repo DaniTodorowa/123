96e530bd05ff1a748a62c3725b06a094c7bfd507
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-15 23:27:26 +0300

    commit

--- JS ADVANCED/04_DOM_lab/02_collect_list_items/CollectListItems.js
+++ JS ADVANCED/04_DOM_lab/02_collect_list_items/CollectListItems.js
@@ -1,7 +1,14 @@
-function extractText() {
-   let itemNodes = document.querySelectorAll('ul#items li');
-   let textarea = document.querySelector("#result");
-   for(let node of itemNodes){
-       textarea.value += node.textContent + "\n";
-   }
+// function extractText() {
+//    let itemNodes = document.querySelectorAll('ul#items li');
+//    let textarea = document.querySelector("#result");
+//    for(let node of itemNodes){
+//        textarea.value += node.textContent + "\n";
+//    }
+// }
+function  extractText(){
+    let elements = document.querySelectorAll('li');
+    let elementsText = Array.from(elements).map(e=>e.textContent);
+   let result = document.getElementById('result');
+   result.textContent = elementsText.join('\n');
+
 }
