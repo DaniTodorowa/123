b493f1ecd9110a655121078a277048266df791a5
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-16 00:09:53 +0300

    commit

--- JS ADVANCED/04_DOM_lab/03_sum_numbers/SumNumbers.js
+++ JS ADVANCED/04_DOM_lab/03_sum_numbers/SumNumbers.js
@@ -1,6 +1,14 @@
-function calc() {
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
-    let result = Number(num1)+Number(num2);
-    document.getElementById("sum").value = result;
+// function calc() {
+//     let num1 = document.getElementById('num1').value;
+//     let num2 = document.getElementById('num2').value;
+//     let result = Number(num1)+Number(num2);
+//     document.getElementById("sum").value = result;
+// }
+
+function calc(){
+    let first = document.getElementById('num1').value;
+    let sec = document.getElementById('num2').value;
+    res = Number(first)+ Number(sec);
+    document.getElementById('sum').value= res;
+
 }
