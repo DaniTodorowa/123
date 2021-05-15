4945f8759671666ede532f4a79bfd73dad127a3e
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-16 00:53:52 +0300

    commit

--- JS ADVANCED/04_DOM_lab/05_colorize_table/05.Colorize Table/ColorizeTable.js
+++ JS ADVANCED/04_DOM_lab/05_colorize_table/05.Colorize Table/ColorizeTable.js
@@ -1,6 +1,12 @@
-function colorize() {
-const rows = document.querySelectorAll('table tr');
-    for(let i=1;i<rows.length;i+=2) {
-        rows[i].style.background= 'teal';
+// function colorize() {
+// const rows = document.querySelectorAll('table tr');
+//     for(let i=1;i<rows.length;i+=2) {
+//         rows[i].style.background= 'teal';
+//     }
+// }
+function colorize(){
+    let rows = document.querySelectorAll('table tr');
+    for(let i=1;i<rows.length;i+=2){
+        rows[i].style.backgroundColor = "teal";
     }
 }
