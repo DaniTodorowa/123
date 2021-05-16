a30d687a1e2058d451293170000042448d49e06e
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-17 00:46:52 +0300

    commit

---
+++ JS ADVANCED/05_DOM_Manipulations_lab/01. List-Of-Items/app.js
@@ -0,0 +1,20 @@
+function addItem() {
+   let inputElement = document.getElementById('newItemText');
+   let liElement = document.createElement('li');
+   liElement.innerHTML = inputElement.value;
+   inputElement.value = "";
+   let itemsListElements = document.getElementById('items');
+   itemsListElements.appendChild(liElement);
+
+
+}
+
+// function addItem() {
+//    let inputEl = document.getElementById('newItemText');
+//    let newLiEl = document.createElement('li');
+//    newLiEl.innerHTML = inputEl.value;
+//    inputEl.value ="";
+//    let itemsLiAll = document.getElementById('items');
+//    itemsLiAll.appendChild(newLiEl);
+//
+// }
