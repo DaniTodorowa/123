7712f4a03a8b0645d11708b81f201ee832bc318d
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-20 21:05:36 +0300

    commit

--- JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/06_cooking_by_numbers.js
+++ JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/06_cooking_by_numbers.js
@@ -1,61 +1,63 @@
-function solve(arr = []) {
-    let num = Number(arr.shift());
-    let res = [];
-    while (arr.length > 0) {
-        let command = arr.shift();
-        switch (command) {
-            case "chop":
-                num = num/2;
-                 res.push(num);
-                 break;
-            case  "dice":
-                num = Math.sqrt(num);
-                 res.push(num);
-                 break;
-            case "spice":
-                num += 1
-                 res.push(num);
-                 break;
-            case "bake":
-                num *= 3;
-                 res.push(num);
-                 break;
-            default :
-                num *= .8
-                 res.push(num);
-                 break;
-        }
-
-    }
-    console.log(res.join("\n"));
-}
-
-(solve(['32', 'chop', 'chop', 'chop', 'chop', 'chop']));
-
-// function solve(...input) {
-//     let arr = input;
+// function solve( ...input) {
 //     let num = Number(input.shift());
-//     arr.forEach(x=>{
-//         switch (x){
+//     let res = [];
+//     while (input.length > 0) {
+//         let command = input.shift();
+//         switch (command) {
 //             case "chop":
-//                 num /= 2
-//                 console.log(num);
-//                 break;
-//             case "dice":
+//                 num = num/2;
+//                  res.push(num);
+//                  break;
+//             case  "dice":
 //                 num = Math.sqrt(num);
-//                 console.log(num);
-//                 break;
+//                  res.push(num);
+//                  break;
 //             case "spice":
-//                 num += 1;
-//                 console.log(num); break;
+//                 num += 1
+//                  res.push(num);
+//                  break;
 //             case "bake":
 //                 num *= 3;
-//                 console.log(num);break;
-//             default:
-//                 num *= 0.8;
-//                 console.log(num.toFixed(1));break;
+//                  res.push(num);
+//                  break;
+//             default :
+//                 num *= .8
+//                  res.push(num);
+//                  break;
 //         }
-//     })
+//
+//     }
+//     console.log(res.join("\n"));
 // }
 //
-// solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
+// (solve(['32', 'chop', 'chop', 'chop', 'chop', 'chop']));
+
+function solve(...input) {
+    let arr = input;
+    let num = Number(input.shift());
+    let res =[]
+    arr.forEach(x=>{
+        switch (x){
+            case "chop":
+                num /= 2
+                res.push(num);
+                break;
+            case "dice":
+                num = Math.sqrt(num);
+                res.push(num);
+                break;
+            case "spice":
+                num += 1;
+                res.push(num); break;
+            case "bake":
+                num *= 3;
+                res.push(num);break;
+            default:
+                num *= 0.8;
+                res.push(num.toFixed(1));break;
+        }
+    })
+    console.log(res.join('\n'));
+}
+
+solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
