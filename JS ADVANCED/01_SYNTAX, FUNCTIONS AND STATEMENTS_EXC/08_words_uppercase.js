d497428210a7501ba37d92e58805e262532cac26
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-21 19:15:02 +0300

    commit

--- JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/08_words_uppercase.js
+++ JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/08_words_uppercase.js
@@ -2,7 +2,7 @@ function solve(input) {
     //console.log(input);
     let result =[]
     let res = [];
-    let re = /\w+/g; //???(/[\W]+/)
+    let re = /[a-zA-Z0-9]+/g
     let found = '';
     let arr = input.split(' ');
     arr.forEach(r=>{
@@ -16,7 +16,18 @@ function solve(input) {
         elNew = el[0].toUpperCase();
         result.push(elNew);
     }
-    console.log(result.join(", "));
+    console.log(result.join(", ")); // this solving gives 66/100
 }
+/*function wordUpperCase(input) {
+    return input.toUpperCase().match(/\w+/gim).join(', ');
+}*/    //100-100 in Judge
+
+// function wordsUppercase(text) {
+//     let result = text.toUpperCase()
+//         .match(/\w+/g)
+//         .join(', ');
+//
+//     console.log(result);         // 100 in Judge
+// }
 
 solve('hello')
