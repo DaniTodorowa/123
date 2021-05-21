6ec114efa953c9a94e7f99e3cab303607666c959
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-21 14:48:48 +0300

    66/100 in judge

---
+++ JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/08_words_uppercase.js
@@ -0,0 +1,22 @@
+function solve(input) {
+    //console.log(input);
+    let result =[]
+    let res = [];
+    let re = /\w+/g;
+    let found = '';
+    let arr = input.split(' ');
+    arr.forEach(r=>{
+        found = r.match(re);
+        res.push(found);
+
+    });
+    //console.log(res);
+    let elNew ;
+    for(let el of res){
+        elNew = el[0].toUpperCase();
+        result.push(elNew);
+    }
+    console.log(result.join(", "));
+}
+
+solve('hello')
