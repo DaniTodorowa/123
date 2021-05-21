1aa9789aa9ea4283fbf1c5c20c2a7bd737fbc756
DaniTodorowa <danitodorova2106@gmail.com>
2021-05-21 20:17:26 +0300

    commit

--- JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/08_words_uppercase.js
+++ JS ADVANCED/01_SYNTAX, FUNCTIONS AND STATEMENTS_EXC/08_words_uppercase.js
@@ -43,3 +43,14 @@ function words(str) {
 }
 
 words('hello')
+
+// function wordUppercase(text){
+//     text=text.toUpperCase().split(/\W/g)
+//     let arr=[]
+//     for (const el of text) {
+//         if(el.length>0){
+//             arr.push(el)
+//         }
+//     }
+//     console.log(arr.join(', '))
+// }
