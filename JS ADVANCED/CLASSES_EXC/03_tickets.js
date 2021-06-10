66bada9cfc7d6e8b16f3f07b87e7d26a4e924502
DaniTodorowa <danitodorova2106@gmail.com>
2021-06-11 00:15:57 +0300

    commit

--- JS ADVANCED/CLASSES_EXC/03_tickets.js
+++ JS ADVANCED/CLASSES_EXC/03_tickets.js
@@ -34,6 +34,7 @@ console.log(solve(['Philadelphia|94.20|available',
         'New York City|95.99|available',
         'New York City|95.99|sold',
         'Boston|126.20|departed'],
-    'destination'
+    'status'
+
 ));
 
