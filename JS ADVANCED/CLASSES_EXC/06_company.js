c549773fe4af6bc886a3861154e55acaf83a9b99
DaniTodorowa <danitodorova2106@gmail.com>
2021-06-13 01:10:43 +0300

    commit

--- JS ADVANCED/CLASSES_EXC/06_company.js
+++ JS ADVANCED/CLASSES_EXC/06_company.js
@@ -58,9 +58,9 @@ class Company {
 let c = new Company();
 c.addEmployee("Stanimir", 2000, "engineer", "Human resources")
 c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
-// c.addEmployee("Slavi", 500, "dyer", "Construction");
-// c.addEmployee("Stan", 2000, "architect", "Construction");
-// c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
-// c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
-// c.addEmployee("Gosho", 1350, "HR", "Human resources");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
 console.log(c.bestDepartment());
