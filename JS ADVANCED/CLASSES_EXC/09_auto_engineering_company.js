433cb09bd2be48358620bdd1721285e2589365c0
DaniTodorowa <danitodorova2106@gmail.com>
2021-06-13 16:43:08 +0300

    commit

--- JS ADVANCED/CLASSES_EXC/09_auto_engineering_company.js
+++ JS ADVANCED/CLASSES_EXC/09_auto_engineering_company.js
@@ -32,3 +32,30 @@ solve(['Audi | Q7 | 1000',
     'Citroen | C4 | 22',
     'Citroen | C5 | 10']
 );
+
+function solving(arr) {
+    let objCars = {};
+    for (let el of arr) {
+        let [name, model, count] = el.split(" | ");
+        count = Number(count);
+        if (!objCars[name]) {
+            objCars[name] = {}
+        }
+        if (!objCars[name][model]) {
+            objCars[name][model] = 0
+
+        }
+        objCars[name][model] += count
+    }
+    //console.log(objCars);
+    let result = '';
+    for(let [name, modelsAndCounts] of Object.entries(objCars)){
+        result+=`${name}\n`;
+        for(let [model, count] of Object.entries(modelsAndCounts)){
+            result += `###${model} -> ${count}\n`
+        }
+
+    }
+    return result;
+
+}
